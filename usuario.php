6ff40e4d8323eae34dd878fb98acd1b970292e9a
Marlon Martos Quiroz <marlonmartos@hotmail.com>
2013-06-27 22:20:57 -0500

    formulario personas
    formulario personas

--- usuario.php
+++ usuario.php
@@ -101,17 +101,7 @@ $('.cc-container').ccvalidate({ onvalidate: function(isValid) {
     </div>
 
   <div class="content">
-    <h1>Instrucciones</h1>
-    <p>Tenga en cuenta que la CSS para estos diseños cuenta con numerosos comentarios. Si realiza la mayor parte de su trabajo en la vista Diseño, eche un vistazo al código para obtener sugerencias sobre cómo trabajar con la CSS para diseños fijos. Puede quitar estos comentarios antes de lanzar el sitio. Para obtener más información sobre las técnicas usadas en estos diseños CSS, lea este artículo en el Centro de desarrolladores de Adobe: <a href="http://www.adobe.com/go/adc_css_layouts">http://www.adobe.com/go/adc_css_layouts</a>.</p>
-    <h2>Método de borrado</h2>
-    <p>Dado que todas las columnas son flotantes, este diseño usa una declaración clear:both en la regla .footer. Esta técnica de borrado fuerza a .container a conocer dónde terminan las columnas con el fin de mostrar cualquier borde o color de fondo que coloque en .container. Si su diseño exige la eliminación de .footer de .container, deberá usar un método de borrado diferente. El más fiable consiste en añadir una &lt;br class=&quot;clearfloat&quot; /&gt; o &lt;div  class=&quot;clearfloat&quot;&gt;&lt;/div&gt; tras la última columna flotante (pero antes de que se cierre .container). Esto proporcionará el mismo efecto de borrado.</p>
-    <h3>Sustitución de logotipo</h3>
-    <p>Se ha utilizado un marcador de posición de imagen en el .header de este diseño, en el que lo más probable es que desee colocar un logotipo. Se recomienda quitar el marcador de posición y reemplazarlo por su propio logotipo vinculado. </p>
-    <p> Tenga en cuenta que si utiliza el inspector de propiedades para navegar hasta la imagen de su logotipo empleando el campo Origen (en lugar de quitar y reemplazar el marcador de posición), deberá quitar el fondo en línea y mostrar las propiedades. Estos estilos en línea sólo se utilizan para hacer que el marcador de posición del logotipo aparezca en los navegadores para fines de demostración. </p>
-    <p>Para quitar los estilos en línea, asegúrese de que el panel Estilos CSS está configurado como Actual. Seleccione la imagen y, en la sección Propiedades del panel Estilos CSS, haga clic con el botón derecho del ratón y elimine las propiedades de visualización y de fondo. (Por supuesto que siempre podrá ir directamente al código y eliminar allí los estilos en línea de la imagen o el marcador de posición.)</p>
-    <h4>Fondos</h4>
-    <p>Por naturaleza, el color de fondo de cualquier div sólo se muestra a lo largo del contenido. Esto significa que si está usando un color de fondo o un borde para crear el aspecto de una columna lateral, éste no se extenderá hasta el pie de página, sino que se detendrá donde termine el contenido. Si la div de .content siempre va a incluir más contenido, puede colocar un borde en la div de .content para separarla de la columna.</p>
-    
+   
      
      <div class="eightcol last">
 
@@ -124,115 +114,80 @@ $('.cc-container').ccvalidate({ onvalidate: function(isValid) {
                                 <label>
                                     Card Type</label>
                             </td>
-                            <td style="width: 270px">
-                                <div class="cc-ddl">
-                                    <div class="cc-ddl-o">
-                                        <select id="cc-types" class="cc-ddl-type">
-                                            <option value="mcd">Master Card</option>
-                                            <option value="vis">Visa Card</option>
-                                            <option value="amx">American Express</option>
-                                            <option value="dnr">Diner Club</option>
-                                            <option value="dis">Discover</option>
-                                        </select>
-                                    </div>
-                                    <div class="cc-ddl-header">
-                                        <div class="cc-ddl-title">Master Card</div>
-                                    </div>
-                                    <div class="cc-ddl-contents" style="display: none;">
-                                        <a href="javascript:return void(0)">Master Card</a> <a href="javascript:return void(0)">
-                                            Visa Card</a> <a href="javascript:return void(0)">American Express</a> <a href="javascript:return void(0)">
-                                                Diners Club</a> <a href="javascript:return void(0)">Discover</a>
-                                    </div>
-                                </div>
-                            </td>
+                            <td style="width: 270px">&nbsp;</td>
                         </tr>
-                        <tr>
-                            <td>
-                                <label>
-                                    Card Number</label>
-                            </td>
-                            <td>
-                                <input type="text" id="card-number" class="large cc-card-number">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label>
-                                    Expires on</label>
-                            </td>
-                            <td>
-                                <div class="cc-ddl" style="width: 110px; float: left;">
-                                    <div class="cc-ddl-o">
-                                        <select id="cc-month">
-                                            <option value="January">January</option>
-                                            <option value="February">February</option>
-                                            <option value="March">March</option>
-                                            <option value="April">April</option>
-                                            <option value="May">May</option>
-                                            <option value="June">June</option>
-                                            <option value="July">July</option>
-                                            <option value="August">August</option>
-                                            <option value="September">September</option>
-                                            <option value="October">October</option>
-                                            <option value="November">November</option>
-                                            <option value="December">December</option>
-                                        </select>
-                                    </div>
-                                    <div class="cc-ddl-header">
-                                        <div class="cc-ddl-title">
-                                                            October</div>
-                                    </div>
-                                    <div class="cc-ddl-contents" style="width: 108px; display: none;">
-                                        <a href="javascript:return void(0)">January</a> <a href="javascript:return void(0)">
-                                            February</a> <a href="javascript:return void(0)">March</a> <a href="javascript:return void(0)">
-                                                April</a> <a href="javascript:return void(0)">May</a> <a href="javascript:return void(0)">
-                                                    June</a> <a href="javascript:return void(0)">July</a> <a href="javascript:return void(0)">
-                                                        August</a> <a href="javascript:return void(0)">September</a> <a href="javascript:return void(0)">
-                                                            October</a> <a href="javascript:return void(0)">November</a>
-                                        <a href="javascript:return void(0)">December</a>
-                                    </div>
-                                </div>
-                                <div class="cc-ddl" style="width: 80px; float: left; margin-left: 4px;">
-                                    <div class="cc-ddl-o">
-                                        <select id="Select1">
-                                            <option value="2011">2011</option>
-                                            <option value="2012">2012</option>
-                                            <option value="2013">2013</option>
-                                            <option value="2014">2014</option>
-                                            <option value="2015">2015</option>
-                                        </select>
-                                    </div>
-                                    <div class="cc-ddl-header">
-                                        <div class="cc-ddl-title">2013</div>
-                                    </div>
-                                    <div class="cc-ddl-contents" style="width: 78px; display: none;">
-                                        <a href="javascript:return void(0)">2011</a> <a href="javascript:return void(0)">2012</a>
-                                        <a href="javascript:return void(0)">2013</a> <a href="javascript:return void(0)">2014</a>
-                                        <a href="javascript:return void(0)">2015</a>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label>
-                                    CVV</label>
-                            </td>
-                            <td>
-                                <input type="text" class="small">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                &nbsp;
-                            </td>
-                            <td>
-                                <input type="submit" value="Checkout" class="cc-checkout" id="check-out">
-                            </td>
-                        </tr>
+             
+                   
+                    
                     </tbody></table>
-                </div>
-
+                
+                    <table>
+                    <tbody>
+                    <form name="form1" method="post" action="">
+                      <p>
+                        <label for="nombre_usuario">Nombre</label>
+                        <input type="text" name="cPerNombres" id="cPerNombres_txt">
+                      </p>
+                      <p>
+                        <label for="apellido_usuario">Apellido</label>
+                        <input type="text" name="cPerApellidos" id="cPerApellidos_txt">
+                      </p>
+                      <p>
+                        <label for="apellido_usuario">DNI</label>
+                        <input type="text" name="nPerDNI" id="nPerDNI_txt">
+                      </p>
+                  
+                      <p>
+                        <label for="dPerNacimiento">dPerNacimiento</label>
+                        <input type="text" name="dPerNacimiento" id="dPerNacimiento_txt">
+                      </p>
+                      <p>
+                        <label for="nDepaCodigo">dPerNacimiento</label>
+                        <input type="text" name="nDepaCodigo" id="nDepaCodigo_txt">
+                      </p>
+                      <p>
+                        <label for="nDepaCodigo">nProvCodigo</label>
+                        <input type="text" name="nProvCodigo" id="nProvCodigo_txt">
+                      </p>
+                      <p>
+                        <label for="nDisCodigo">nDisCodigo</label>
+                        <input type="text" name="nDisCodigo" id="nDisCodigo_txt">
+                      </p>
+                      <p>
+                        <label for="nCPCodigo">nCPCodigo</label>
+                        <input type="text" name="nCPCodigo" id="nCPCodigo_txt">
+                      </p>
+                        <p>Caserio  
+                        <label for="nCPCodigo">nCasCodigo</label>
+                        <input type="text" name="nCasCodigo" id="nCasCodigo_txt">
+                      </p>
+                      <p>
+                      <p>
+                        <label for="cPerDireccion">Direccion</label>
+                      <input type="text" name="cPerDireccion" id="cPerDireccion_txt">
+                                                   
+                      </p>
+                      <p>
+                        <label for="cPerReferencia">Referencia</label>
+                      <input type="text" name="cPerReferencia" id="cPerReferencia_txt">
+                                                   
+                      </p>
+                      <p><label for="cPerReferencia">Estados</label>
+                          <div id="estados" >
+                               
+                                   <br>En Lista <input type="checkbox" id="bPerLista" name="bPerLista" value="1" />
+                       <br>Enviado <input type="checkbox" id="bPerEnviado" name="bPerEnviado" value="1" />
+                        <br>                  RecibePago <input type="checkbox" id="bPerRecibePago" name="bPerRecibePago" value="1" />         
+                           <br>               bPerObservado <input type="checkbox" id="bPerObservado" name="bPerObservado" value="1" />  
+                          </div>       
+                      </p>
+                    </form>
+                    </tbody>
+                        </table>
+</div>
+ <h1>Instrucciones</h1>
+    <p>Tenga en cuenta que la CSS para estos diseños cuenta con numerosos comentarios. Si realiza la mayor parte de su trabajo en la vista Diseño, eche un vistazo al código para obtener sugerencias sobre cómo trabajar con la CSS para diseños fijos. Puede quitar estos comentarios antes de lanzar el sitio. Para obtener más información sobre las técnicas usadas en estos diseños CSS, lea este artículo en el Centro de desarrolladores de Adobe: <a href="http://www.adobe.com/go/adc_css_layouts">http://www.adobe.com/go/adc_css_layouts</a>.</p>
+ 
     <!-- End Form -->
 
   </div>
